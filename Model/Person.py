0ff89ff9a5272e1632fe7019e56d4b5624d3c0d0
Lemongate <116230630+Holocoaster@users.noreply.github.com>
2022-11-23 23:22:30 +0000

    Exhibition Structure

---
+++ Model/Person.py
@@ -0,0 +1,19 @@
+class Person:
+
+  #Attributes
+
+ id = None
+ name = None
+
+  #Constructor method
+
+def __init__(self, id, name):
+  self.id = id
+  self.name = name
+
+  #Exhibition 
+
+  def __str__(self):
+   return f"{self.name} ({self.id})"
+  
+  
---
+++ main.py
@@ -0,0 +1,10 @@
+#Importing Person file in the Model directory
+
+from Model.Person import Person
+
+#Usage
+Human = Person(1,"Paul")
+print(Human)
+
+
+
